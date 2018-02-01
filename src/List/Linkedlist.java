6cf45224dbc5c0fad381bf0971e6b8c442cce4d1
Yoshinobibi <fox_zz@126.com>
2018-02-01 16:46:58 +0800

    2018.02.01

--- src/List/Linkedlist.java
+++ src/List/Linkedlist.java
@@ -3,7 +3,7 @@ package List;
 public class Linkedlist {
 
     Node head = null;
-    class Node{
+    static class Node{
         Node next = null;
         int data;
         public Node(int data){
@@ -21,63 +21,86 @@ public class Linkedlist {
         return length;
     }
     //增加节点
-    public void addNode(int d){
-        Node newNode = new Node(d);
-        //空链表则增加为head
-        if (head == null){
-            head = newNode;
+    public void addNode(Node node){
+        Node temp = head;
+        if (head == null) {
+            head = node;
             return;
         }
-        //非空链表增加到尾部
-        Node temp = head;
         while (temp.next != null){
             temp = temp.next;
         }
-        temp.next = newNode;
+        temp.next = node;
+    }
+    //插入节点到指定位置
+    public void addNodeByIndex(int index,Node node) {
+        Node temp = head;
+        if (index < 1 || index > length()+1) {
+            System.out.println("addNodeByIndex error");
+            return;
+        }
+        for (int j = 1; j < index; j++){
+            temp = temp.next;
+        }
+        node.next = temp.next;
+        temp.next = node;
     }
     //删除第i个节点
-    public boolean deleteNode(int i){
-        if (i < 1 || i > length()){
-            return false;
+    public void deleteNodeByIndex(int index) {
+        Node temp = head;
+        if (index < 1 || index > length()) {
+            System.out.println("deleteNodeByIndex error");
+            return;
         }
-        //node i-1 => node i+1
-        int j = 2;
-        Node preNode = head;
-        Node curNode = preNode.next;
-        while (curNode != null) {
-            if (j == i) {
-                preNode.next = curNode.next;
-                return true;
-            }
-            preNode = curNode;
-            curNode = curNode.next;
-            j++;
+        if (index == 1) {
+            head = head.next;
+            return;
         }
-        return false;
+        for (int j = 2; j < index; j++) {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
     }
     //打印链表
     public void printList() {
-        Node tmp = head;
-        while (tmp != null) {
-            System.out.println(tmp.data);
-            tmp = tmp.next;
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+    //反转链表 将node i 指向node i-1
+    public void reverseList() {
+        Node temp = head;
+        while (temp != null) {
+            temp = temp.next;
         }
     }
 
     public static void main(String[] args) {
         Linkedlist list = new Linkedlist();
-        list.addNode(5);
-        list.addNode(3);
-        list.addNode(1);
-        list.addNode(2);
-        list.addNode(55);
-        list.addNode(36);
+        Node node1 = new Node(1);
+        Node node2 = new Node(12);
+        Node node3 = new Node(21);
+        Node node4 = new Node(31);
+        Node node5 = new Node(41);
+
+        list.addNode(node1);
+        list.addNode(node2);
+        list.addNode(node3);
+        list.addNode(node4);
+        list.addNode(node5);
+
         list.printList();
-        System.out.println("linkLength:" + list.length());
-        System.out.println("head.data:" + list.head.data);
-        list.deleteNode(3);
-        System.out.println("After deleteNode(4):");
+        System.out.println("------------------------");
+//        list.deleteNodeByIndex(1);
+//
+//        list.printList();
+
+        list.addNodeByIndex(1,new Node(4));
         list.printList();
+
+
     }
 
 
